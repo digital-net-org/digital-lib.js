74e0776a3ceae2b8640818142d412b630d9fe10e
BenoitSafari <gregory.michalak.192@gmail.com>
2025-02-03 00:35:25 +0100

    feat(next): Added next support

---
+++ packages/core/models/entities/index.ts
@@ -0,0 +1,6 @@
+export type { Entity } from './Entity';
+export type { EntitySchema, EntitySchemaProperty } from './EntitySchema';
+export type { EntityRaw } from './EntityRaw';
+export { default as EntityHelper } from './EntityHelper';
+export { default as EntitySchemaHelper } from './EntitySchemaHelper';
+export * from './Views';
--- packages/core/models/index.ts
+++ packages/core/models/index.ts
@@ -1,10 +1,6 @@
-export type { Entity } from './Entity';
-export type { EntitySchema, EntitySchemaProperty } from './EntitySchema';
-export type { EntityRaw } from './EntityRaw';
-export { default as EntityHelper } from './EntityHelper';
-export { default as EntitySchemaHelper } from './EntitySchemaHelper';
 export type { PatchOperation, Patch } from './Patch';
 export type { Position } from './Position';
 export type { ResultMessage } from './ResultMessage';
 export type { Result } from './Result';
 export type { QueryResult } from './QueryResult';
+export * from './entities';
