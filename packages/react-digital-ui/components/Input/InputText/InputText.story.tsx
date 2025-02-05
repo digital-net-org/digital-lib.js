c20f87faa7957e5e912ba98272f1ed92b5523b1e
BenoitSafari <gregory.michalak.192@gmail.com>
2025-02-05 20:45:38 +0100

    fix(storybook): InputText fixed story state tests

--- packages/react-digital-ui/components/Input/InputText/InputText.story.tsx
+++ packages/react-digital-ui/components/Input/InputText/InputText.story.tsx
@@ -2,20 +2,42 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import InputText, { type InputTextProps } from './InputText';
+import { Box } from '../../Box';
 
 const meta: Meta<InputTextProps> = {
     title: 'Inputs/InputText',
     component: InputText,
-    decorators: (Story, { args }) => {
-        const [value, setValue] = React.useState('');
-        return (
-            <Story {...args} onChange={setValue} value={value} />
-        );
-    },
 };
 
 type Story = StoryObj<typeof meta>;
 export default meta;
+
+export const StateTests: Story = {
+    decorators: () => {
+        const [value, setValue] = React.useState('');
+        return (
+            <Box gap={2}>
+                <InputText type="text" onChange={setValue} value={value} />
+                <Box>
+                    State content:
+                    <pre>
+                        {JSON.stringify(value, null, 2)}
+                    </pre>
+                </Box>
+            </Box>
+        );
+    },
+    args: {
+        type: 'text',
+        pattern: undefined,
+        patternMessage: 'Invalid input',
+        required: false,
+        loading: false,
+        disabled: false,
+        fullWidth: false,
+        borderless: false,
+    },
+};
 
 export const Primary: Story = {
     argTypes: {
