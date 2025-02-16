fc9f05db562b2200c72e1e14a0f707202ac6738c
BenoitSafari <gregory.michalak.192@gmail.com>
2025-02-16 15:08:54 +0100

    feat(components): Application default logo

--- packages/react-digital-ui/chunks/Logo/Logo.tsx
+++ packages/react-digital-ui/chunks/Logo/Logo.tsx
@@ -1,10 +1,9 @@
-import { Text } from '../../components';
 import './Logo.styles.css';
 
 export default function Logo() {
     return (
         <div className="DigitalUi-AppLogo">
-            <Text variant="h1">Digital.net - Pages</Text>
+            <img src="/assets/logo.png" />
         </div>
     );
 }
